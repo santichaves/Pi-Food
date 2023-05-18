8810f64e7334ff280b46f6550b4a4f2d4619ab25
santichaves <Santy_chaves95@hotmail.com>
2023-05-18 16:27:20 -0300

    Cambio

--- api/index.js
+++ api/index.js
@@ -19,13 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require('dotenv').config();
-const {PORT}= process.env
+
 
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at' PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
